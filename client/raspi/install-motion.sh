0fc885b6f9b6c0e7281162fe833d4206b5b3907d
Dennis Nyström <dennis.nystrom@gmail.com>
2018-01-18 03:25:22 +0100

    scripts for raspi

--- client/raspi/install-motion.sh
+++ client/raspi/install-motion.sh
@@ -5,5 +5,10 @@ sudo apt-get dist-upgrade -y
 wget github.com/Motion-Project/motion/releases/download/release-4.0.1/pi_jessie_motion_4.0.1-1_armhf.deb 
 
 sudo apt-get install gdebi-core -y
-sudo gdebi pi_jessie_motion_4.0.1-1_armhf.deb -y
+sudo gdebi pi_jessie_motion_4.0.1-1_armhf.deb
 
+#
+# Don't forget:
+# - Add auto start to /etc/rc.local: 
+#   motion -c /home/pi/apps/parking-guard-guard/client/raspi/motion.conf
+#
